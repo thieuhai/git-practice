1a65e59ba0524af4d73a36e5a34e8d0e6034d58a
mdmahmudurrahman <md.mahmudur.rahman.bd@gmail.com>
2018-03-18 13:42:30 +0700

    Chapter 6

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
 
   resources :users
+  	
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'	
+  delete '/logout', to: 'sessions#destroy'
 end
