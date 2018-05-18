0992acf0b4233ba7130d41749b0af15b3c5b9be2
tdtrung5893 <tranducha23081967@gmail.com>
2018-05-18 20:20:06 +0700

    Edit application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  def hello
-    render html: "Hello, world!"
-  end
-
   def logged_in_user
     unless logged_in?
       store_location
-      flash[:danger] = "Please log in."
+      flash[:danger] = "Đăng nhập trước khi sử dụng tính năng này!"
       redirect_to login_url
     end
   end
