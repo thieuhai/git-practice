f76605f5dcf3a12c695e239866f8901b3c0af82e
HungNguyenUET <hungnguyenuet@gmail.com>
2018-05-18 20:19:38 +0700

    Edit application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  include SessionsHelper
+  include SesionsHelper
 
   def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Đăng nhập trước khi sử dụng tính năng này!"
-      redirect_to login_url
-    end
+    return if logged_in?
+    store_location
+    flash[:danger] = "Đăng nhập trước khi sử dụng tính năng này!"
+    redirect_to login_url
   end
 end
