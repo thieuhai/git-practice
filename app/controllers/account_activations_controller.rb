bfb36d0bc28a5c34cf20b739613c5a9c827c274b
shjn0 <nguyentung.pdu@gmail.com>
2018-05-18 20:25:31 +0700

    acc_activations_controller
    2nd commit
    
    combine

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,13 +2,12 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by email: params[:email]
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.update_attributes activated: true
-      user.update_attributes activated_at: Time.zone.now
+      user.update_attributes activated: true, activated_at: Time.zone.now
       log_in user
-      flash[:success] = "Account activated!"
+      flash[:success] = "Tài khoản đã được kích hoạt thành công!"
       redirect_to user
     else
-      flash[:danger] = "Invalid activation link"
+      flash[:danger] = "Đường dẫn kích hoạt không hợp lệ"
       redirect_to root_url
     end
   end
